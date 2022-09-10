34e14a5165a6ead2a8a85dd215647a1b8c0b9b52
Majo <max-josef.overlack@hsrw.org>
2022-09-10 19:48:05 +0000

    Add xml_to_fdb()

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use assembly::fdb::{
-    common::Latin1String, common::ValueType, core::Field, mem::Database, sqlite::try_export_db,
-    store,
+use assembly::{
+    fdb::{
+        common::Latin1String,
+        common::ValueType,
+        core::{self, Field},
+        mem::Database,
+        sqlite::try_export_db,
+        store,
+    },
+    xml::{
+        common::{expect_decl, expect_end},
+        database::{
+            self, expect_column_or_end_columns, expect_columns, expect_database,
+            expect_row_or_end_rows, expect_rows,
+        },
+        quick::Reader,
+    },
 };
 use mapr::Mmap;
 use rusqlite::{types::ValueRef, Connection};
-use std::{fs::File, io::BufWriter, process::abort, time::Instant};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufReader, BufWriter},
+    process::abort,
+    time::Instant,
+};
 
 fn main() {}
 
@@ -166,4 +186,139 @@ pub extern "C" fn sqlite_to_fdb() {
     );
 
     println!("Output written to 'output.fdb'");
+}
+
+/// Code adapted from
+/// https://github.com/LUDevNet/Assembly/blob/dd2a7e0e9494cc94f7cc4df814f2631d1af1f306/modules/data/examples/xmldb-to-fdb.rs#L38
+#[no_mangle]
+pub extern "C" fn xml_to_fdb() {
+    let src_file = File::open("input.xml").unwrap();
+    let reader = BufReader::new(src_file);
+
+    let dest_file = File::create("output.fdb").unwrap();
+    let mut dest_out = BufWriter::new(dest_file);
+
+    println!("Copying file, this may take a few seconds...");
+    let start = Instant::now();
+
+    let mut dest_db = store::Database::new();
+
+    let mut xml = Reader::from_reader(reader);
+    let xml = xml.trim_text(true);
+
+    let mut buf = Vec::new();
+    let buf = &mut buf;
+
+    expect_decl(xml, buf).unwrap();
+    let db_name = expect_database(xml, buf).unwrap().unwrap();
+    println!("Loading database: '{}'", db_name);
+
+    while let Some(table_name) = expect_database(xml, buf).unwrap() {
+        println!("table '{}'", table_name);
+        let mut dest_table = store::Table::new(128);
+
+        expect_columns(xml, buf).unwrap();
+
+        let mut col_map = HashMap::new();
+
+        while let Some(col) = expect_column_or_end_columns(xml, buf).unwrap() {
+            let data_type = match col.r#type {
+                database::ValueType::Bit => ValueType::Boolean,
+                database::ValueType::Float => ValueType::Float,
+                database::ValueType::Real => ValueType::Float,
+                database::ValueType::Int => ValueType::Integer,
+                database::ValueType::BigInt => ValueType::BigInt,
+                database::ValueType::SmallInt => ValueType::Integer,
+                database::ValueType::TinyInt => ValueType::Integer,
+                database::ValueType::Binary => ValueType::Text,
+                database::ValueType::VarBinary => ValueType::Text,
+                database::ValueType::Char => ValueType::Text,
+                database::ValueType::VarChar => ValueType::Text,
+                database::ValueType::NChar => ValueType::Text,
+                database::ValueType::NVarChar => ValueType::Text,
+                database::ValueType::NText => ValueType::VarChar,
+                database::ValueType::Text => ValueType::VarChar,
+                database::ValueType::Image => ValueType::VarChar,
+                database::ValueType::DateTime => ValueType::BigInt,
+                database::ValueType::Xml => ValueType::VarChar,
+                database::ValueType::Null => ValueType::Nothing,
+                database::ValueType::SmallDateTime => ValueType::Integer,
+            };
+            if col_map.is_empty() {
+                // first col
+                if data_type == ValueType::Float {
+                    let id_col_name = format!("{}ID", table_name);
+                    dest_table.push_column(Latin1String::encode(&id_col_name), ValueType::Integer);
+                    col_map.insert(id_col_name, col_map.len());
+                }
+            }
+
+            dest_table.push_column(Latin1String::encode(&col.name), data_type);
+            col_map.insert(col.name, col_map.len());
+        }
+
+        expect_rows(xml, buf).unwrap();
+
+        let col_count = dest_table.columns().len();
+        let mut auto_inc = 0;
+        while let Some(row) = expect_row_or_end_rows(xml, buf, true).unwrap() {
+            let mut fields = vec![core::Field::Nothing; col_count];
+            let mut pk = None;
+            for (key, src_value) in row {
+                let col_index = *col_map.get(&key).unwrap();
+                let value_type = dest_table.columns().get(col_index).unwrap().value_type();
+                let dest_value = match value_type {
+                    ValueType::Nothing => core::Field::Nothing,
+                    ValueType::Integer => core::Field::Integer(src_value.parse().unwrap()),
+                    ValueType::Float => core::Field::Float(src_value.parse().unwrap()),
+                    ValueType::Text => core::Field::Text(src_value),
+                    ValueType::Boolean => core::Field::Boolean(&src_value != "0"),
+                    ValueType::BigInt => core::Field::BigInt(src_value.parse().unwrap()),
+                    ValueType::VarChar => core::Field::VarChar(src_value),
+                };
+
+                if col_index == 0 {
+                    match &dest_value {
+                        core::Field::Integer(i) => {
+                            pk = Some((*i % 128) as usize);
+                        }
+                        core::Field::BigInt(i) => {
+                            pk = Some((*i % 128) as usize);
+                        }
+                        core::Field::Text(text) => {
+                            let lat1 = Latin1String::encode(text);
+                            pk = Some((lat1.hash() % 128) as usize);
+                        }
+                        core::Field::VarChar(var_char) => {
+                            let lat1 = Latin1String::encode(var_char);
+                            pk = Some((lat1.hash() % 128) as usize);
+                        }
+                        _ => panic!("Can't use {:?} as PK", &dest_value),
+                    }
+                }
+
+                fields[col_index] = dest_value;
+            }
+            let pk = if let Some(pk) = pk {
+                pk
+            } else {
+                auto_inc += 1;
+                fields[0] = core::Field::Integer(auto_inc);
+                (auto_inc as usize) % 128
+            };
+            dest_table.push_row(pk, &fields);
+        }
+
+        expect_end(xml, buf, "table").unwrap();
+        dest_db.push_table(Latin1String::encode(&table_name), dest_table);
+    }
+
+    dest_db.write(&mut dest_out).unwrap();
+
+    let duration = start.elapsed();
+    println!(
+        "Finished in {}.{}s",
+        duration.as_secs(),
+        duration.subsec_millis()
+    );
 }
